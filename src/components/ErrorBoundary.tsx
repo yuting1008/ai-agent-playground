589d217f4beeadcea4e4d5e3d60a65e0af6d0737
theodore <theodore.niu@gmail.com>
2024-12-09 23:27:54 +0800

    catch error

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -6,6 +6,7 @@ interface ErrorBoundaryProps {
 
 interface ErrorBoundaryState {
   hasError: boolean;
+  errorMessage: string;
 }
 
 class ErrorBoundary extends React.Component<
@@ -14,12 +15,12 @@ class ErrorBoundary extends React.Component<
 > {
   constructor(props: any) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { hasError: false, errorMessage: '' };
   }
 
   static getDerivedStateFromError(error: Error) {
     console.error('error', error);
-    return { hasError: true };
+    return { hasError: true, errorMessage: error.message };
   }
 
   componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
@@ -29,15 +30,46 @@ class ErrorBoundary extends React.Component<
   render() {
     if (this.state.hasError) {
       return (
-        <h1>
-          Something went wrong.
-          {this.state.hasError.toString()}
-        </h1>
+        <div style={styles.container}>
+          <div>Something Went Wrong</div>
+          <div>{this.state.errorMessage}</div>
+          <div style={styles.reload} onClick={() => window.location.reload()}>
+            Reload
+          </div>
+        </div>
       );
     }
 
     return this.props.children;
   }
 }
+
+const styles: { [key: string]: React.CSSProperties } = {
+  reload: {
+    cursor: 'pointer',
+    fontSize: '20px',
+    color: 'white',
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    padding: '10px 20px',
+    borderRadius: '5px',
+  },
+  container: {
+    zIndex: 999999,
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    height: '100vh',
+    fontSize: '30px',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    color: 'white',
+    gap: '20px',
+  },
+};
 
 export default ErrorBoundary;
